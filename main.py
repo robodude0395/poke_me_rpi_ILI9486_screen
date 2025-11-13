adfec054e388587524d9c64acabf6a63cd00f1cd
Omar <robodude20000@gmail.com>
2025-11-13 18:06:32 +0000

    Minor fix in main.py

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ def vote_story():
     return data, 200
 
 if __name__ == "__main__":
+    disp.clear((0, 0, 0))
     app.config['TESTING'] = True
     app.config['DEBUG'] = True
     app.run(debug=True, host="0.0.0.0", port=5000)
